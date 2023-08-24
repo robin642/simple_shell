b9c4fdc807df5c8b87f7ece18906e05cd7c19e75
robin642 <wrobindean@gmail.com>
2023-08-24 05:53:05 -0700

    HEADER FILE

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+
+#endif
